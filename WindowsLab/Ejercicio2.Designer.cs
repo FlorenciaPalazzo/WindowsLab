d10fe5ac520c28029170631a64be0aaeb70e72e9
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-10-20 14:54:42 -0300

    Ejercicio2-Finalizado

--- WindowsLab/Ejercicio2.Designer.cs
+++ WindowsLab/Ejercicio2.Designer.cs
@@ -28,44 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEdad = new System.Windows.Forms.Button();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnEdad
             // 
-            this.button1.Location = new System.Drawing.Point(229, 171);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEdad.Location = new System.Drawing.Point(219, 169);
+            this.btnEdad.Name = "btnEdad";
+            this.btnEdad.Size = new System.Drawing.Size(105, 23);
+            this.btnEdad.TabIndex = 0;
+            this.btnEdad.Text = "Calcular Edad";
+            this.btnEdad.UseVisualStyleBackColor = true;
+            this.btnEdad.Click += new System.EventHandler(this.btnEdad_Click);
             // 
-            // label1
+            // lblEdad
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(250, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(234, 97);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(70, 13);
+            this.lblEdad.TabIndex = 1;
+            this.lblEdad.Text = "Ingrese Edad";
             // 
-            // textBox1
+            // txtEdad
             // 
-            this.textBox1.Location = new System.Drawing.Point(219, 126);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtEdad.Location = new System.Drawing.Point(219, 126);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 2;
             // 
             // Ejercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 316);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.btnEdad);
             this.Name = "Ejercicio2";
             this.Text = "Ejercicio2";
             this.ResumeLayout(false);
@@ -75,8 +76,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEdad;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtEdad;
     }
 }
--- WindowsLab/Ejercicio2.cs
+++ WindowsLab/Ejercicio2.cs
@@ -16,5 +16,31 @@ namespace WindowsLab
         {
             InitializeComponent();
         }
+
+        private void btnEdad_Click(object sender, EventArgs e)
+        {
+            int edad = Convert.ToInt32(txtEdad.Text);
+
+            bool edadCero= edad == 0;
+            bool menor18 = edad < 18;
+            bool mayor18 = (edad > 18) && (edad<60);
+            bool mayor60 = edad >= 60;
+
+            if(edad == 0 )
+            {
+                MessageBox.Show("ERROR");
+            }
+             else if(edad < 18)
+            {
+                MessageBox.Show("menor de edad");
+            }else if ((edad > 18) && (edad < 60))
+            {
+                MessageBox.Show("mayor de edad");
+            }else if (edad >= 60)
+            {
+                MessageBox.Show("adulto mayor");
+            }
+
+        }
     }
 }
